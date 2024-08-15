b8458a1da199c43996e9c3b874d97bc134a62179
leo <chichali374@gmail.com>
2024-08-16 01:04:05 +0800

    精選文章區塊完成

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  slidesPerGroup: 2,
+  spaceBetween: 48,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination-custom",
+    type: "fraction"
+  },
+  breakpoints: {
 
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+});
 console.log("Hello world!");
