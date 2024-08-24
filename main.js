30ee854b6ff49e3da2801e311d943bb5a85fd91e
leo <chichali374@gmail.com>
2024-08-24 21:58:31 +0800

    品味生活區塊完成

--- main.js
+++ main.js
@@ -19,7 +19,26 @@ const swiper = new Swiper(".featuredArticle", {
     },
   },
 });
-
+//品味生活
+const swiper0 = new Swiper(".life", {
+  slidesPerView: 1,
+  slidesPerGroup: 0,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-next1",
+    prevEl: ".swiper-prev1"
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+    },
+  },
+});
 //文章內容頁
 const swiper3 = new Swiper(".extendArticle", {
   slidesPerView: 1,
